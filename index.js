87b11d59174e66040128abfd68eb93c74e38c3d3
Miranda Little <129182008+MirandaLittle@users.noreply.github.com>
2024-09-18 14:24:29 +0200

    derde commit

--- index.js
+++ index.js
@@ -57,80 +57,84 @@ let element = "";
 
 //findMatches function
 
-const findMatches = (potentialMatches, candidateBirthday) => potentialMatches.forEach(person => {
+const findMatches = (potentialMatches, candidateBirthday) => {
+  const matches = [];
+  potentialMatches.forEach(person => {
 
-  const birthdayCandidate = massageDate(candidateBirthday);
-  const birthdayMatch = massageDate(person.birthday.dmy);
+    const birthdayCandidate = massageDate(candidateBirthday);
+    const birthdayMatch = massageDate(person.birthday.dmy);
 
-  const determineZodiac = date => {
-    if ((date.month === 3 && date.day >= 21) || (date.month === 4 && date.day < 21)) {
-      zodiac = zodiacArray[0];
-    } else if ((date.month === 4 && date.day >= 21) || (date.month === 5 && date.day < 22)) {
-      zodiac = zodiacArray[1];
-    } else if ((date.month === 5 && date.day >= 22) || (date.month === 6 && date.day < 22)) {
-      zodiac = zodiacArray[2];
-    } else if ((date.month === 6 && date.day >= 22) || (date.month === 7 && date.day < 24)) {
-      zodiac = zodiacArray[3];
-    } else if ((date.month === 7 && date.day >= 24) || (date.month === 8 && date.day < 24)) {
-      zodiac = zodiacArray[4];
-    } else if ((date.month === 8 && date.day >= 24) || (date.month === 9 && date.day < 24)) {
-      zodiac = zodiacArray[5];
-    } else if ((date.month === 9 && date.day >= 24) || (date.month === 10 && date.day < 24)) {
-      zodiac = zodiacArray[6];
-    } else if ((date.month === 10 && date.day >= 24) || (date.month === 11 && date.day < 23)) {
-      zodiac = zodiacArray[7];
-    } else if ((date.month === 11 && date.day >= 23) || (date.month === 12 && date.day < 22)) {
-      zodiac = zodiacArray[8];
-    } else if ((date.month === 12 && date.day >= 22) || (date.month === 1 && date.day < 21)) {
-      zodiac = zodiacArray[9];
-    } else if ((date.month === 1 && date.day >= 21) || (date.month === 2 && date.day < 20)) {
-      zodiac = zodiacArray[10];
-    } else if ((date.month === 2 && date.day >= 20) || (date.month === 3 && date.day < 21)) {
-      zodiac = zodiacArray[11];
-    } return zodiac
-  };
+    const determineZodiac = date => {
+      if ((date.month === 3 && date.day >= 21) || (date.month === 4 && date.day < 21)) {
+        zodiac = zodiacArray[0];
+      } else if ((date.month === 4 && date.day >= 21) || (date.month === 5 && date.day < 22)) {
+        zodiac = zodiacArray[1];
+      } else if ((date.month === 5 && date.day >= 22) || (date.month === 6 && date.day < 22)) {
+        zodiac = zodiacArray[2];
+      } else if ((date.month === 6 && date.day >= 22) || (date.month === 7 && date.day < 24)) {
+        zodiac = zodiacArray[3];
+      } else if ((date.month === 7 && date.day >= 24) || (date.month === 8 && date.day < 24)) {
+        zodiac = zodiacArray[4];
+      } else if ((date.month === 8 && date.day >= 24) || (date.month === 9 && date.day < 24)) {
+        zodiac = zodiacArray[5];
+      } else if ((date.month === 9 && date.day >= 24) || (date.month === 10 && date.day < 24)) {
+        zodiac = zodiacArray[6];
+      } else if ((date.month === 10 && date.day >= 24) || (date.month === 11 && date.day < 23)) {
+        zodiac = zodiacArray[7];
+      } else if ((date.month === 11 && date.day >= 23) || (date.month === 12 && date.day < 22)) {
+        zodiac = zodiacArray[8];
+      } else if ((date.month === 12 && date.day >= 22) || (date.month === 1 && date.day < 21)) {
+        zodiac = zodiacArray[9];
+      } else if ((date.month === 1 && date.day >= 21) || (date.month === 2 && date.day < 20)) {
+        zodiac = zodiacArray[10];
+      } else if ((date.month === 2 && date.day >= 20) || (date.month === 3 && date.day < 21)) {
+        zodiac = zodiacArray[11];
+      } return zodiac
+    };
 
-  const zodiacMatch = determineZodiac(birthdayMatch);
-  const zodiacCandidate = determineZodiac(birthdayCandidate);
-  // console.log(zodiacMatch);
-  // console.log(zodiacCandidate);
+    const zodiacMatch = determineZodiac(birthdayMatch);
+    const zodiacCandidate = determineZodiac(birthdayCandidate);
+    // console.log(zodiacMatch);
+    // console.log("zodiac candidate:" + zodiacCandidate);
 
-  const determineElement = zodiac => {
-    if (fire.includes(zodiac)) {
-      element = "Fire";
-    } if (earth.includes(zodiac)) {
-      element = "Earth";
-    } if (air.includes(zodiac)) {
-      element = "Air";
-    } if (water.includes(zodiac)) {
-      element = "Water";
-    } return element
-  }
+    const determineElement = zodiac => {
+      if (fire.includes(zodiac)) {
+        element = "Fire";
+      } if (earth.includes(zodiac)) {
+        element = "Earth";
+      } if (air.includes(zodiac)) {
+        element = "Air";
+      } if (water.includes(zodiac)) {
+        element = "Water";
+      } return element
+    }
 
-  const elementMatch = determineElement(zodiacMatch);
-  const elementCandidate = determineElement(zodiacCandidate);
-  // console.log(elementMatch);
-  // console.log(elementCandidate);
+    const elementMatch = determineElement(zodiacMatch);
+    const elementCandidate = determineElement(zodiacCandidate);
+    // console.log(elementMatch);
+    // console.log("element candidate " + elementCandidate);
 
-  // determine matches
-  // compare element of candidate with elements of randomPersons
+    // determine matches
+    // compare element of candidate with elements of randomPersons
 
-  const determineMatch = (elementCandidate, elementMatch) => {
-    if ((elementCandidate === "Fire") && (elementMatch === "Fire" || "Air")) {
-      matches.push({ "first_name": person.name, "last_name": person.surname, "zodiac": zodiacMatch, "age": person.age });
-    } else if ((elementCandidate === "Earth") && (elementMatch === "Earth" || "Water")) {
-      matches.push({ "first_name": person.name, "last_name": person.surname, "zodiac": zodiacMatch, "age": person.age });
-    } else if ((elementCandidate === "Air") && (elementMatch === "Air" || "Fire")) {
-      matches.push({ "first_name": person.name, "last_name": person.surname, "zodiac": zodiacMatch, "age": person.age });
-    } else if ((elementCandidate === "Water") && (elementMatch === "Earth" || "Water")) {
-      matches.push({ "first_name": person.name, "last_name": person.surname, "zodiac": zodiacMatch, "age": person.age });
-    } return matches
-  };
+    const determineMatch = (elementCandidate, elementMatch) => {
+      if ((elementCandidate === "Fire") && (elementMatch === "Fire" || "Air")) {
+        matches.push({ "first_name": person.name, "last_name": person.surname, "zodiac": zodiacMatch, "age": person.age });
+      } else if ((elementCandidate === "Earth") && (elementMatch === "Earth" || "Water")) {
+        matches.push({ "first_name": person.name, "last_name": person.surname, "zodiac": zodiacMatch, "age": person.age });
+      } else if ((elementCandidate === "Air") && (elementMatch === "Air" || "Fire")) {
+        matches.push({ "first_name": person.name, "last_name": person.surname, "zodiac": zodiacMatch, "age": person.age });
+      } else if ((elementCandidate === "Water") && (elementMatch === "Earth" || "Water")) {
+        matches.push({ "first_name": person.name, "last_name": person.surname, "zodiac": zodiacMatch, "age": person.age });
+      } return matches
+    };
+    determineMatch(elementCandidate, elementMatch);
 
-  determineMatch(elementCandidate, elementMatch);
+  });
+
   return matches;
 
-});
+};
 
 const birthdayCandidateMassaged = massageDate(birthdayCandidate);
 const determineAge = birthday => {
@@ -145,9 +149,10 @@ const determineAge = birthday => {
 };
 
 const personsMassaged = randomPersonData.map(updateAge);
-console.log(personsMassaged);
+// console.log(personsMassaged);
+// console.log(matches);
 const matchesUnsorted = findMatches(personsMassaged, birthdayCandidate);
-console.log("Matchesunsorted " + matchesUnsorted)
+// console.log("Matchesunsorted " + matchesUnsorted)
 const candidateAge = determineAge(birthdayCandidateMassaged);
 
 
@@ -158,11 +163,8 @@ const compareAgeDifference = (matchA, matchB) => {
   let ageDifferenceMatchB = candidateAge - matchB.age;
   if (ageDifferenceMatchA < 0) {
     ageDifferenceMatchA = ageDifferenceMatchA * -1;
-    console.log(ageDifferenceMatchA);
-
   } if (ageDifferenceMatchB < 0) {
     ageDifferenceMatchB = ageDifferenceMatchB * -1;
-    console.log(ageDifferenceMatchB);
   } if (ageDifferenceMatchA > ageDifferenceMatchB) {
     return 1
   } if (ageDifferenceMatchA < ageDifferenceMatchB) {
@@ -171,17 +173,26 @@ const compareAgeDifference = (matchA, matchB) => {
   return 0
 };
 
-const matchA = { "name": "Jake", "age": 33 };
-const matchB = { "name": "Vanessa", "age": 42 };
-console.log(compareAgeDifference(matchA, matchB)); // -1 matchA difference is smaller
+// const matchA = { "name": "Jake", "age": 33 };
+// const matchB = { "name": "Vanessa", "age": 42 };
+// console.log(compareAgeDifference(matchA, matchB)); // -1 matchA difference is smaller
 
-console.log(matchesUnsorted.sort(compareAgeDifference));
-// const matchesSorted = matchesUnsorted.sort(compareAgeDifference)
+// console.log(matchesUnsorted.sort(compareAgeDifference));
+const matchesSorted = matchesUnsorted.sort(compareAgeDifference)
+// console.log(matchesSorted.slice(0, 20));
 
-// const displayMatches = matchesSorted.forEach(person => {
+const first20Matches = matchesSorted.slice(0, 20);
+
+const printPerson = person => {
+  console.log(`Name: ${person.first_name} ${person.last_name}`);
+  console.log(`Zodiac: ${person.zodiac}`);
+  console.log(`Age: ${person.age}`);
+  console.log("-------------------------------------------");
 
 
-// });
+};
+
+first20Matches.forEach(printPerson);
 
 
 // console.log(randomPersonData[0]);
